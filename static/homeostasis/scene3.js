8c0091b64ba7d214dda84b6201acf40b7a84b28c
S.J. Zhang <43072514+s-j-zhan9@users.noreply.github.com>
2019-12-18 21:35:03 -0500

    0.9

--- static/homeostasis/scene3.js
+++ static/homeostasis/scene3.js
@@ -15,6 +15,7 @@ class SceneThree {
     render(sketch) {
 
         let hue;
+        let maxVal;
 
         sketch.setup = function () {
             let canvas = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
@@ -23,7 +24,7 @@ class SceneThree {
             // Move the canvas so it’s inside our <div id="sketch-holder">.
             canvas.parent('sketch-holder3');
 
-            sketch.colorMode(sketch.HSB, 360, 100, 100, 100);
+            sketch.colorMode(sketch.HSB, 360, 160, 100, 100);
             sketch.noStroke();
 
             sketch.background(0, 100, 100);
@@ -34,10 +35,15 @@ class SceneThree {
             }else{
                 hue = sketch.int(sketch.random(200, 240));
             }
+
+            maxVal = 160;
+
+            sketch.frameRate(30);
         };
 
+        let sat;
+
         sketch.draw = function () {
-            sketch.background(hue, 80, 80);
 
             let tb = sketch.floor(sketch.rotationX);
 
@@ -49,6 +55,15 @@ class SceneThree {
                 tb = 90;
             }
 
+            if(document.getElementById('view6').style.display !== "none"){
+                if(sketch.frameCount % 30 == 0){
+                    maxVal--;
+                }
+            }
+
+            sat = sketch.map(Math.abs(tb), 0, 90, 0, maxVal);
+
+            sketch.background(hue, sat, 100);
 
 
             if (sketch.frameCount % 12 == 0) {
--- static/homeostasis/ui.js
+++ static/homeostasis/ui.js
@@ -15,11 +15,13 @@ window.addEventListener('resize', () => {
 // Receiving commands from controller to change scene
 connection.on('sceneChange', function (data) {
     if (data == 0){
+        transitionToView3();
+    } else if (data == 1){
         transitionToView45();
-    } else if (data = 1){
-        // transitionToView6();
     } else if (data == 2) {
-        // transitionToView7();
+        transitionToView55();
+    } else if (data == 3) {
+        transitionToEnding();
     }
 });
 
@@ -37,22 +39,28 @@ function onLoad(){
     var view1 = document.getElementById("view1");
     var view2 = document.getElementById("view2");
     var selectRoom = document.getElementById("selectRoom");
+    var waitingRoom = document.getElementById("waitingRoom");
+
     var view3 = document.getElementById("view3");
     var view4 = document.getElementById("view4");
     var view45 = document.getElementById("view45");
     var view5 = document.getElementById("view5");
     var view55 = document.getElementById("view55");
     var view6 = document.getElementById("view6");
+    var ending = document.getElementById("ending");
+
+
 
     view2.style.display = "flex";
     selectRoom.style.display = "none";
+    waitingRoom.style.display = "none";
     view3.style.display = "none";
     view4.style.display = "none";
     view45.style.display = "none";
     view5.style.display = "none";
     view55.style.display = "none";
     view6.style.display = "none";
-
+    ending.style.display = "none";
 }
 
 function transitionToView2(){
@@ -61,13 +69,42 @@ function transitionToView2(){
 }
 
 function transitionToSelectRoom(){
-    view2.style.display = "none";
+    view2.classList.add("fade-out");
     selectRoom.style.display = "flex";
+    var delayInMilliseconds = 1000; 
+    setTimeout(function() {
+        view2.style.display = "none";
+    }, delayInMilliseconds);
+}
+
+function transitionToWaitingRoom(){
+    selectRoom.classList.remove("fade-in");
+    selectRoom.classList.add("fade-out");
+    waitingRoom.style.display = "flex";
+    var delayInMilliseconds = 1000; 
+    setTimeout(function() {
+        selectRoom.style.display = "none";
+    }, delayInMilliseconds);
 }
 
 function transitionToView3(){
-    selectRoom.style.display = "none";
+    waitingRoom.classList.add("fade-out");
     view3.style.display = "flex";
+
+    //hide all scenes
+    selectRoom.style.display = "none";
+    waitingRoom.style.display = "none";
+    view45.style.display = "none";
+    view5.style.display = "none";
+    view55.style.display = "none";
+    view6.style.display = "none";
+    ending.style.display = "none";
+
+
+    var delayInMilliseconds = 1000; 
+    setTimeout(function() {
+        selectRoom.style.display = "none";
+    }, delayInMilliseconds);
 
     var delayInMilliseconds = 4000; //1 second
     setTimeout(function() {
@@ -76,29 +113,103 @@ function transitionToView3(){
 }
 
 function transitionToView4(){
-    view3.style.display = "none";
+    view3.classList.add("fade-out");
     view4.style.display = "flex";
+
+    var delayInMilliseconds = 1000; 
+    setTimeout(function() {
+        view3.style.display = "none";
+    }, delayInMilliseconds);
+
 }
 function transitionToView45(){
-    view4.style.display = "none";
-    view45.style.display = "flex";
+
+    //hide all scenes
+    selectRoom.style.display = "none";
+    waitingRoom.style.display = "none";
+    view3.style.display = "none";
+    view55.style.display = "none";
+    view6.style.display = "none";
+    view2.style.display = "none";
+    ending.style.display = "none ";
+
+    view4.classList.add("fade-out-4");
+    setTimeout(function(){
+        view4.style.display = "none";
+        view45.style.display = "flex";
+    }, 1000);
+
     setTimeout(function(){ transitionToView5() }, 4000);
 }
 
 function transitionToView5(){
-    view45.style.display = "none";
-    view5.style.display = "flex";
-
+    view45.classList.add("fade-out");
+    setTimeout(function(){
+        view45.style.display = "none";
+        view5.style.display = "flex";
+        }, 1000);
 }
 
 function transitionToView55(){
-    view5.style.display = "none";
+
+    //hide all scenes
+    selectRoom.style.display = "none";
+    waitingRoom.style.display = "none";
+    view3.style.display = "none";
+    view4.style.display = "none";
+    view45.style.display = "none";
+    view2.style.display = "none";
+    ending.style.display = "none";
+
+    view5.classList.add("fade-out");
+    setTimeout(function(){
+        view5.style.display = "none";
+    }, 1000);
+    
     view55.style.display = "flex";
-    setTimeout(function(){ transitionToView6() }, 4000);
+
+    setTimeout(function(){
+         transitionToView6() 
+        }, 4000);
 }
 
 function transitionToView6(){
-    view55.style.display = "none";
-    view6.style.display = "flex";
+    view55.classList.add("fade-out");
+    setTimeout(function(){
+        view55.style.display = "none";
+        view6.style.display = "flex";
+
+    }, 1000);
+}
+
+function transitionToEnding(){
+        //hide all scenes
+        selectRoom.style.display = "none";
+        waitingRoom.style.display = "none";
+        view3.style.display = "none";
+        view4.style.display = "none";
+        view45.style.display = "none";
+        view5.style.display = "none";
+        view55.style.display = "none";
+        view2.style.display = "none";
+
+    view6.classList.add("fade-out");
+    setTimeout(function(){
+        view6.style.display = "none";
+        ending.style.display = "flex";
+    }, 1000);
+
+
+
+    setTimeout(function(){
+        ending.classList.add("fade-out");
+
+    }, 3000);
+    setTimeout(function(){
+        window.location.href = '/credit.html'
+
+    }, 4000);
+
+
 }
 
